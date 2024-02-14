b0fae87756d798c66eaf92978bbf1ab086ed6646
administrator <Chittina@mail.uc.edu>
2024-02-14 13:03:41 -0500

    Update Homepage

---
+++ email.js
@@ -0,0 +1,11 @@
+var shown = false;
+function showhideEmail(){
+	if (shown){
+		document.getElementById('email').innerHTMl = "Show my email";
+	shown=false;
+	}else{
+	var myemail = "<a href = 'mailto:chittina"+"@"+"ucmail.uc.edu'>chittina"+"@"+"ucmail.uc.edu</a>";
+	document.getElementById('email').innerHTML=myemail;
+	shown = true;
+}
+}
